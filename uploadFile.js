a6631678814129fa6075774e31b6207d5c561b3a
00Rohith00 <rohith10012002@gmail.com>
2023-12-06 15:12:51 +0530

    first commit

--- uploadFile.js
+++ uploadFile.js
@@ -2,17 +2,16 @@
 const multer = require('multer');
 const path = require('path');
 
-fileName ="";
 var storage  = multer.diskStorage(
   {
       destination : 'uploads',  
       filename    : function(req, file ,cb)
                     {
-                        fileName = Date.now() + path.extname(file.originalname);
-                        cb(null,fileName);
+                        cb(null,Date.now() + path.extname(file.originalname));
                     }
   });
 
+const fileName = storage.filename;
 const uploadFile = multer({storage : storage}).single('profile');
 
 module.exports = { uploadFile , fileName }
